d5e588fd6abbfb191aa846afce91f4601d3af201
wanchenQu <wanchen_qu@163.com>
2021-07-21 14:57:09 +0800

    wanchenQu <T2.java> change test

--- cloud-gateway-gateway9527/src/test/java/T2.java
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -4,6 +4,8 @@ public class T2 {
     public static void main(String[] args) {
         ZonedDateTime zonedDateTime = ZonedDateTime.now();
         System.out.println(zonedDateTime);
+        System.out.println(zonedDateTime);
+
     }
 
     // 2021-07-19T17:36:36.287+08:00[Asia/Shanghai]
