0f634ff57d6c84f705ea087e58fcb3ade4be7933
wanchenQu <wanchen_qu@163.com>
2021-07-21 15:20:32 +0800

    wanchenQu <T2.java> push test

--- cloud-gateway-gateway9527/src/test/java/T2.java
+++ cloud-gateway-gateway9527/src/test/java/T2.java
@@ -8,6 +8,7 @@ public class T2 {
         System.out.println(zonedDateTime + "third test");
         System.out.println(zonedDateTime + "master modify");
         System.out.println(zonedDateTime + "hotfix branch modify");
+        System.out.println(zonedDateTime + "push test");
 
     }
 
