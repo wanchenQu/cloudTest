9dd436d5c9ad69f8d45ef6100cc94d54431d49f5
wanchenQu <wanchen_qu@163.com>
2021-07-22 16:43:53 +0800

    wanchenQu 20210722 BUS, RabbitMQ

--- cloud-config-client3366/src/main/java/com/chenTest/springcloud/controller/ConfigClientController.java
+++ cloud-config-client3366/src/main/java/com/chenTest/springcloud/controller/ConfigClientController.java
@@ -8,17 +8,20 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RefreshScope
 /**
- * 能够自动的获悉刷新的内容，让运维工程师发送POST请求刷新3355。
- * 我们还需要让运维人员发送一下POST请求，刷新一下3355
- * curl -X POST "http://localhost:3355/actuator/refresh"
+ * 能够自动的获悉刷新的内容，让运维工程师发送POST请求刷新3366。
+ * 我们还需要让运维人员发送一下POST请求，刷新一下3366
+ * curl -X POST "http://localhost:3366/actuator/refresh"
  * */
 public class ConfigClientController {
 
     @Value("${config.info}")
     private String configInfo;
 
+    @Value("${server.port}")
+    private String serverPort;
+
     @GetMapping("/configInfo")
     public String getConfigInfo() {
-        return configInfo;
+        return configInfo + "serverPort: " + "\n" + serverPort;
     }
 }
